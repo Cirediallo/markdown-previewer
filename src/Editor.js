10198a457e85d7dbad8b4833638c6265407c8d1d
Cirediallo <theevilmonster93@gmail.com>
2019-08-15 21:46:06 +0200

    Marked work and text transformation work in Markdown

--- src/Editor.js
+++ src/Editor.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './StyleApp.css';
 import ReactMarkdown from 'react-markdown';
 
+const marked = require('marked')
+
 export default class Editor extends React.Component{
     constructor(props){
         super(props);
 
         this.state = {
-            input: ''
+            input: placeholder
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -42,9 +44,55 @@ class Preview extends React.Component{
 
     render(){
         return (
-            <div id="preview">
-                {this.props.input}
-            </div>
+            <div id="preview" dangerouslySetInnerHTML = {{__html: marked(this.props.input)}} />
         )
     }
 }
+
+
+const placeholder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`;
